8e88002eeeaac98759e5f87ed56343ff89b08b03
Ali Rezai <ali.rezai27461@gmail.com>
2025-08-25 11:49:46 +0200

    first commit

---
+++ src/main/java/se/moln/ecommerceintegration/ECommerceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package se.moln.ecommerceintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceIntegrationApplication.class, args);
+    }
+
+}
