61b2d54ce3b0e66597c3edc435fb631706d14a40
Ali Rezai <ali.rezai27461@gmail.com>
2025-08-26 14:08:01 +0200

    change openapiconfig

--- src/main/java/se/moln/ecommerceintegration/config/OpenApiConfig.java
+++ src/main/java/se/moln/ecommerceintegration/config/OpenApiConfig.java
@@ -5,14 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI userServiceOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://moln.ittt.se")))
                 .info(new Info()
                     .title("User Service API")
                     .version("v1")
