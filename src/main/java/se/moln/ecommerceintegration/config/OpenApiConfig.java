8fec2c718d199b653a941f03bc9d5fc207f4e7d0
Ali Rezai <ali.rezai27461@gmail.com>
2025-08-26 11:06:59 +0200

    me

--- src/main/java/se/moln/ecommerceintegration/config/OpenApiConfig.java
+++ src/main/java/se/moln/ecommerceintegration/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package se.moln.ecommerceintegration.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,19 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI userServiceOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("User Service API")
-                .version("v1")
-                .description("Auth & User endpoints"));
+        return new OpenAPI()
+                .info(new Info()
+                    .title("User Service API")
+                    .version("v1")
+                    .description("Auth & User endpoints"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth")
+                        .addList("apiKeyAuth"))
+                .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
